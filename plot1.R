2fc54cc5be593e8784bbe294b30509f6a6100016
Steven Post <steven.storckpost@gmail.com>
2014-05-07 13:24:50 -0500

    New code with easter eggs

--- plot1.R
+++ plot1.R
@@ -1,3 +1,9 @@
+##### GREETINGS PEER REVIEWER! I hope you're enjoying this course. I am even though I think the course could use some ... tweaking
+##### Thanks for taking the time to look at this. I realize that I have unnecessarily changed the name of my data twice but I did
+#### that during development so that if it blew up (which it did, repeatedly) I wouldn't have to read in the whole damn 2 mega-records again
+
+
+
 hp<-read.csv("household_power_consumption.txt",header=TRUE,sep=";",na.strings="?") ### read in the file
 hpc=hp                                                                            ### rename it to cut down on chance of having to reload 
 hpc$Date=as.Date(hpc$Date,"%d/%m/%Y")                                             ### put date into date format
@@ -7,23 +13,13 @@ hist(hpco$Global_active_power,col="red",ylab="Frequency",xlab="Global Active Pow
 dev.off()
 
 
+#### Here's a dumb joke I heard yesterday just for fun. It may or may not make sense/be funny depending on what part of the galaxy 
+#### you're from:
+#### Q: What day is Star Wars Day?
+#### A: May the Fourth.
+####  (I said it was dumb, but how many other jokes are you going to read at github today?)
 
 
 
 
 
-###### code above here, play below IGNORE EVERYTHING BELOW; it is just my notes that helped me create the file and I don't want to lose them
-hpctest=hpco[1:5,]
-hpctest
-hpco=subset(hpc,Date=="2007-02-01"|Date=="2007-02-02")
-tail(hpco)
-str(hpco)
-hpctest$Time=as.Time(hpctest$Time)
-hptest=hp[1:5,]
-hptest
-hptest$Date=cbind(hptest$Date,hptest$Time)
-hist(hpco$Global_active_power,col="red",ylab="Frequency",xlab="Global Active Power (kilowatts)",main="Global Active Power")
-png(filename = "Rplot%03d.png",
-    width = 480, height = 480, units = "px", pointsize = 12,
-    bg = "white", res = NA, family = "", restoreConsole = TRUE,
-    type = c("windows", "cairo", "cairo-png"), antialias
